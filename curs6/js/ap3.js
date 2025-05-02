290576b2a90d8babdb541c0a92aa38938c24890d
anamariagiurgea <anamaria.giurgea@mail.com>
2025-05-02 11:43:47 +0300

    module

---
+++ curs6/js/ap3.js
@@ -0,0 +1,12 @@
+// 1. definim o functie
+let adunare = (a, b) => {  
+    return a + b;
+}
+// function adunare(a,b){
+//     return a + b;
+// }
+
+// 2. apelam functia
+console.log("Suma dintre 24 si 46 este", adunare(24, 46));
+
+// 3. in terminal rulam comanda: node ap3.js
